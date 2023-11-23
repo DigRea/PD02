cd25cc883748338c00fc3b02ea0179aba2a5211e
DigRea <146025236+DigRea@users.noreply.github.com>
2023-11-23 14:53:05 +0300

    Initial commit

---
+++ variables.py
@@ -0,0 +1,21 @@
+# Объект - Автомобиль
+
+# Строковый тип
+mark = 'Mazda'
+# Строковый тип
+model = '6'
+# Число с плавающей запятой
+volume = 2.0
+# Промежуточные данные
+gasoline = True
+diesel = False
+# Логический тип
+fuel = gasoline
+# Целочисленный тип
+year = 2006
+
+print(type(mark))
+print(type(model))
+print(type(volume))
+print(type(fuel))
+print(type(year))
